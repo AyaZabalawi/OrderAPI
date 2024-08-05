2d923f09a58116f31eea6512c3e5ecec5ac2ff9e
AyaZabalawi <aya.mzabalawi@gmail.com>
2024-08-05 18:00:21 +0400

    Add project files.

---
+++ ServiceContracts/IOrderItemsService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Logging;
+using ServiceContracts.DTO;
+
+namespace OrderAPI.Services
+{
+    public interface IOrderItemsService
+    {
+        Task<OrderItemResponse> AddOrderItem(OrderItemAddRequest orderItemRequest);
+        Task<bool> DeleteOrderItemByOrderItemId(Guid orderItemId);
+        Task<List<OrderItemResponse>> GetAllOrderItems();
+        Task<List<OrderItemResponse>> GetOrderItemsByOrderId(Guid orderId);
+        Task<OrderItemResponse?> GetOrderItemByOrderItemId(Guid orderItemId);
+        Task<OrderItemResponse> UpdateOrderItem(OrderItemUpdateRequest orderItemRequest);
+    }
+}
